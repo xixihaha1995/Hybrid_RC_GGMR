b5554155d1642de54dc3b30e219b10a5842ff4e3
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-03-26 20:49:44 -0600

    time for re-optimizing case 5

--- RC/_1_utils.py
+++ RC/_1_utils.py
@@ -603,7 +603,7 @@ def swarm_plot(y_train, y_train_pred, y_test, y_test_pred, swarm_constants):
     ax[0].plot(y_train_pred, label='modeled')
 
     ax[0].set_title(
-        f'Train, from {0}th mins to {(start-1) * minutes_interval}th mins, CVRMSE:{cv_rmse(y_train, y_train_pred):.2f},MAE:{mae(y_train, y_train_pred):.2f},MAPE:{mean_absolute_percentage_error(y_train, y_train_pred):.2f}')
+        f'Train, from {0}th mins to {(start-1) * minutes_interval}th mins, NRMSE:{nrmse(y_train, y_train_pred):.6f}, CVRMSE:{cv_rmse(y_train, y_train_pred):.2f},MAE:{mae(y_train, y_train_pred):.2f},MAPE:{mean_absolute_percentage_error(y_train, y_train_pred):.2f}')
     ax[0].set_ylabel('Load Power (W)')
     ax[0].set_xlabel(f'Time Step, with {minutes_interval} mins interval')
     if swarm_constants['case_nbr'] == 2:
@@ -611,7 +611,8 @@ def swarm_plot(y_train, y_train_pred, y_test, y_test_pred, swarm_constants):
     ax[1].plot(y_test, label='measured')
     ax[1].plot(y_test_pred, label='modeled')
     ax[1].set_title(
-        figure_title + nl + f'Test, from {start  * minutes_interval}th mins to {(start+ len(y_test) )* minutes_interval}th mins, CVRMSE:{cv_rmse(y_test, y_test_pred):.2f},MAE:{mae(y_test, y_test_pred):.2f}, MAPE:{mean_absolute_percentage_error(y_test, y_test_pred):.2f}')
+        figure_title + nl + f'Test, from {start  * minutes_interval}th mins to {(start+ len(y_test) )* minutes_interval}th mins, '
+                            f'NRMSE:{nrmse(y_test, y_test_pred):.6f},CVRMSE:{cv_rmse(y_test, y_test_pred):.2f},MAE:{mae(y_test, y_test_pred):.2f}, MAPE:{mean_absolute_percentage_error(y_test, y_test_pred):.2f}')
     ax[1].set_ylabel('Load Power (W)')
     ax[1].set_xlabel(f'Time Step, with {minutes_interval} mins interval')
     if swarm_constants['case_nbr'] == 2:
