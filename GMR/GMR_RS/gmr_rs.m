bcb7bc7b7ecf2545424f6850ecabe0783ebf8893
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-01 21:16:02 -0600

    Growing GMR

--- GMR/GMR_RS/gmr_rs.m
+++ GMR/GMR_RS/gmr_rs.m
@@ -5,6 +5,35 @@ nbStates = 14;
 
 %% Convert RC training data to GMR training data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if ~isfile('data/case_arr_sim.mat')
+    T=readtable('data/case_arr.csv');
+    t_out=(T{:,1} - 32) *5/9;
+    t_slabs= (T{:,6} - 32 ) * 5/9;
+    t_cav = (T{:,50} - 32) * 5/ 9;
+    t_water_sup = (T{:,29}-32)*5/9;
+    t_water_ret = (T{:,30} - 32) *5/9;
+    vfr_water = T{:,28};
+    m3_per3_perCFM = 0.00047194745;
+    ahu_cfm1 = T{:,42} *  m3_per3_perCFM;
+    ahu_t_sup1 = (T{:,43}  - 32) * 5/9;
+    ahu_cfm2 = T{:,46} * m3_per3_perCFM;
+    ahu_t_sup2 = (T{:,47} - 32 ) *5/9;
+    
+%     q_solar = T{:,75};
+%     q_light = T{:,81};
+%     q_inte_heat = T{:,79};
+    
+    c_water = 4.186;
+    rho_water = 997e3;
+    gal_per_min_to_m3 = 6.309e-5;
+    y= c_water*rho_water*gal_per_min_to_m3*vfr_water.*(t_water_sup - t_water_ret);
+    
+    data = [t_slabs,t_cav,t_water_sup,t_water_ret,vfr_water,...
+        ahu_cfm1,ahu_t_sup1,ahu_cfm2,ahu_t_sup2,t_out,y];
+    data = data.';
+    save('data/case_arr_sim.mat','data');
+end
+
 if ~isfile('data/case_arr.mat')
     T=readtable('data/case_arr.csv');
     t_out=(T{:,1} - 32) *5/9;
@@ -13,18 +42,21 @@ if ~isfile('data/case_arr.mat')
     t_water_sup = (T{:,29}-32)*5/9;
     t_water_ret = (T{:,30} - 32) *5/9;
     vfr_water = T{:,28};
-    q_solar = T{:,75};
-    q_light = T{:,81};
-    q_inte_heat = T{:,79};
     m3_per3_perCFM = 0.00047194745;
     ahu_cfm1 = T{:,42} *  m3_per3_perCFM;
     ahu_t_sup1 = (T{:,43}  - 32) * 5/9;
     ahu_cfm2 = T{:,46} * m3_per3_perCFM;
     ahu_t_sup2 = (T{:,47} - 32 ) *5/9;
+    
+    q_solar = T{:,75};
+    q_light = T{:,81};
+    q_inte_heat = T{:,79};
+    
     c_water = 4.186;
     rho_water = 997e3;
     gal_per_min_to_m3 = 6.309e-5;
     y= c_water*rho_water*gal_per_min_to_m3*vfr_water.*(t_water_sup - t_water_ret);
+    
     data = [t_slabs,t_cav,t_water_sup,t_water_ret,vfr_water,q_solar,...
         q_light,q_inte_heat,ahu_cfm1,ahu_t_sup1,ahu_cfm2,ahu_t_sup2,t_out,y];
     data = data.';
@@ -32,7 +64,7 @@ if ~isfile('data/case_arr.mat')
 end
 %% Load a dataset consisting of 3 demonstrations of a 2D signal.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-load('data/case_arr.mat'); %load 'Data'
+load('data/case_arr_sim.mat'); %load 'Data'
 total_length = size(data,2);
 training_length = 4032;
 testing_length = total_length - training_length;
@@ -83,3 +115,4 @@ mean_measured = mean(abs(actual_test_y));
 cvrmse = rmse*100 / mean_measured;
 title("CVRMSE is " + cvrmse + "%")
 legend({'Predicted','Actual'},'Location','southwest')
+close all;
---
+++ GMR/GMR_RS/sensitivty_gmr_rs.m
@@ -0,0 +1,8 @@
+% Control parameters sensitivity analysis
+% For each input, select the best state numbers
+% Different input variables
+% Plan 1 All
+% Plan 2 No synthetic data, Radiation
+% Plan 3 No AHU
+% Plan 4 No Water
+% Plan 5 Only Out, Cav, Slab 
