795c190abcdef8fa1736ba70d34b2b8a8d418e3f
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-05 18:31:59 -0600

    Matlab real time called RC

--- Hybrid/GGMR_Call_RC.py
+++ Hybrid/GGMR_Call_RC.py
@@ -1,15 +1,13 @@
-print("Hello")
-import sys
 from RC_Package.RC_training import _2_pyswarm as RC_utils
 from RC_Package.RC_prediction import call_rc_function
-#
-# def recv_send(target_time_idx):
-#     warming_up_steps = 15
-#     u_measured = RC_utils.warming_input_demo(time_idx = target_time_idx, seg_length= warming_up_steps)
-#     rc_function = call_rc_function.RC_Prediction()
-#     rc_y = rc_function.predict(u_measured)
-#     return rc_y
-#
-# target_time_idx = int(sys.argv[1])
-# rc_y = recv_send(target_time_idx)
-# print(f'RC prediction with pre-calcualted abcd:{rc_y} Watts load')
+
+def recv_send(target_time_idx):
+    warming_up_steps = 15
+    u_measured = RC_utils.warming_input_demo(time_idx = target_time_idx, seg_length= warming_up_steps)
+    rc_function = call_rc_function.RC_Prediction()
+    rc_y = rc_function.predict(u_measured)
+    return rc_y
+
+res = recv_send(int(target_time_idx))
+print(f'RC prediction with pre-calcualted abcd:{res} Watts load')
+
--- Hybrid/test_function.m
+++
@@ -1,3 +0,0 @@
-function [cvrmse_gmr, cvrmse_ggmr] = test_function(nbstates)
-cvrmse_gmr = nbstates + 1;
-cvrmse_ggmr = nbstates + 3;
