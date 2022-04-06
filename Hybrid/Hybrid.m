01d51933354b7b841f77167edda52d2eb3883476
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-05 19:31:27 -0600

    Matlab call system py3.8 interpreter

--- Hybrid/Hybrid.m
+++ Hybrid/Hybrid.m
@@ -1,4 +1,3 @@
 disp(["Type the following in Matlab Command Window";...
     "pyenv('Version', 'C:\Users\[Your User Name]\AppData\Local\Programs\Python\Python38\python.exe')"])
-% y = pyrunfile("GGMR_Call_RC.py 782")
 res = pyrunfile("GGMR_Call_RC.py","res",target_time_idx=782)
