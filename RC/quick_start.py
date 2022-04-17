4ed5942aef3bf5fe8b2ac9b1d002fc738281facf
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-04-16 22:42:24 -0600

    without sink node is better

--- RC/quick_start.py
+++ RC/quick_start.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
     rc_obj.train(_test_start= 4033, _state_num=4, _input_num=8,_para_nums=19,_case_nbr=5, _n_particle=2, _iters=2)
     '''
     rc_obj = RC_package.RC_Class()
-    rc_obj.train(_test_start= 4033, _n_particle=500, _iters=50) #training
+    # rc_obj.train(_test_start= 4033, _n_particle=500, _iters=50) #training
     # rc_obj.train(_test_start=4033) #use trained RsCs get predicted
-    # # rc_obj.train(_test_start= 4033, _state_num=4, _input_num=8,_para_nums=19,_case_nbr=5, _n_particle=1000, _iters=20)
-    # # rc_obj.train(_test_start=4033, _state_num=4, _input_num=8, _para_nums=19, _case_nbr=5)
+    rc_obj.train(_test_start= 4033, _state_num=4, _input_num=8,_para_nums=19,_case_nbr=5, _n_particle=1000, _iters=20)
+    # rc_obj.train(_test_start=4033, _state_num=4, _input_num=8, _para_nums=19, _case_nbr=5)
     # # rc_obj.train(_test_start=4033, _state_num=5,_case_nbr=7, _n_particle=1000, _iters=20)
     # rc_obj.train(_test_start=4033, _state_num=5, _case_nbr=7)
 
